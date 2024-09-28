89fdf1b2a2447a676ee756614cc65ac3079e789e
Noah Giles <giles.noah.a@gmail.com>
2024-09-28 18:03:27 +0000

    Implement lobby ID creation

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,14 @@
+import uuid
+
+import redis
+from app.schemas import StudyQuestionResponse
+from app.utils import process_document
 from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.schemas import StudyQuestionResponse
-from app.utils import process_document
-
 app = FastAPI()
+conn = redis.Redis(host="redis")
+
 
 # Allow CORS for frontend
 app.add_middleware(
@@ -25,3 +29,10 @@ async def upload_file(file: UploadFile = File(...)):
     study_questions = process_document(content, file.filename)
     print(study_questions)
     return StudyQuestionResponse(study_questions=study_questions)
+
+
+@app.post("/create-lobby")
+def create_lobby():
+    lobby_id = uuid.uuid4().hex
+    return_val = conn.hset(f"lobby:{lobby_id}", mapping={"creator": "1"})
+    return {"lobby_id": lobby_id}
